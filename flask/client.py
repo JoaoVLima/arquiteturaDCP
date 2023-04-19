5147474770e6ec44b3a175fd88bd027ea3112e47
JoaoVLima <celu.joaolima@gmail.com>
2023-04-19 18:48:21 -0300

    correcoes servidor

--- flask/client.py
+++ flask/client.py
@@ -2,16 +2,25 @@ from requests import get, post, put, delete, patch
 from sys import argv, stderr
 
 def main():
-    if len(argv) < 2:
-        print(f"USO: {argv[0]} <URL> <msg>", file=stderr)
+    if len(argv) <= 2:
+        print(f"USO: {argv[0]} <URL> <conta> <funcao>", file=stderr)
         exit(1)
 
     url = argv[1]
-    msg = " ".join(argv[2:])
-    r = patch(url + "/eco/" + msg).json()
-    print("Resposta:", r['resp'])
-    r = get(url + "/eco").json()
-    print("Contador:", int(r['cont']))
+    conta = argv[2]
+    funcao = None
+    if len(argv) > 3:
+        funcao = argv[3]
+
+    if not funcao:
+        r = get(f'{url}/contas/{conta}').json()
+        print("Resposta:", r)
+    elif funcao == 'deposito' or funcao == 'saque':
+        r = patch(f'{url}/contas/{conta}/{funcao}').json()
+        print("Resposta:", r)
+    else:
+        print(f"USO: {argv[0]} <URL> <conta> <funcao>", file=stderr)
+        exit(1)
 
 if __name__ == '__main__':
     main()
--- flask/servidor.py
+++ flask/servidor.py
@@ -4,27 +4,72 @@ import sys
 
 app = Flask(__name__)
 api = Api(app)
-class Eco:
-    def __init__(self, sufixo):
-        self.sufixo = sufixo
-        self.cont = 0
 
-    def diga(self, msg):
-        self.cont += 1
-        return msg + self.sufixo
+class Banco:
+    def __init__(self, contas=None):
+        if not contas:
+            contas = {'1234-5': Conta('1234-5')}
+        self.contas = contas
+
+    def saldo(self, conta):
+        conta_obj = self.contas.get(conta)
+        if not conta_obj:
+            raise 'Conta não existe'
+        return conta_obj.saldo
+
+    def deposito(self, conta, valor):
+        conta_obj = self.contas.get(conta)
+        if not conta_obj:
+            raise 'Conta não existe'
+        return conta_obj.deposito(valor)
+
+    def saque(self, conta, valor):
+        conta_obj = self.contas.get(conta)
+        if not conta_obj:
+            raise 'Conta não existe'
+        return conta_obj.saque(valor)
+
+
+class Conta:
+    def __init__(self, codigo):
+        self.codigo = codigo
+        self.saldo = 0.0
+
+    def deposito(self, valor):
+        self.saldo += valor
+        return self.saldo
+
+    def saque(self, valor):
+        if self.saldo < valor:
+            raise 'Saldo Insuficiente'
+        self.saldo -= valor
+        return self.saldo
 
 
 
-eco = Eco('_ECO')
-class EcoIF(Resource):
-    def get(self):
-        return {'cont': eco.cont}
+# URL	Resultado
+# http://localhost:1512/contas/1234-5	para obter saldo da conta "1234-5"
+# http://localhost:1512/contas/1234-5/deposito	para efetuar depósito de um valor na conta "1234-5"
+# http://localhost:1512/contas/1234-5/saque	para efetuar o saque de um valor na conta "1234-5" (o saque pode produzir erro, se o saldo for insuficiente)
 
-    def patch(self, msg):
-        return {'resp': eco.diga(msg)}
+banco = Banco()
+
+class SaldoIF(Resource):
+    def get(self, conta):
+        return banco.saldo(conta=conta)
+
+class DepositoIF(Resource):
+    def patch(self, conta):
+        return banco.deposito(conta=conta, valor=1)
+
+class SaqueIF(Resource):
+    def patch(self, conta):
+        return banco.saque(conta=conta, valor=1)
 
 
-api.add_resource(EcoIF, "/eco", "/eco/<string:msg>")
+api.add_resource(SaldoIF, "/contas", "/contas/<string:conta>")
+api.add_resource(DepositoIF, "/contas", "/contas/<string:conta>/deposito")
+api.add_resource(SaqueIF, "/contas", "/contas/<string:conta>/saque")
 
 if __name__ == '__main__':
     port = 1512
